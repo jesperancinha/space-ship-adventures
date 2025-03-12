909ea08493c53b05d0e1db36efcea695ad1db19f
João Esperancinha <jofisaes@gmail.com>
2025-03-12 12:02:30 +0100

    fixes unit tests

--- app/src/test/kotlin/org/jesperancinha/space/DatabasesRoutingKtTest.kt
+++ app/src/test/kotlin/org/jesperancinha/space/DatabasesRoutingKtTest.kt
@@ -2,7 +2,7 @@ package org.jesperancinha.space
 
 import io.ktor.client.request.*
 import io.ktor.server.testing.*
-import org.jesperancinha.space.configureDatabases
+import org.jesperancinha.space.route.configureDatabases
 import kotlin.test.Test
 
 class DatabasesRoutingKtTest {
