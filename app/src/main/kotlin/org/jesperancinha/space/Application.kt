b93ced171264dc099e636e0aa2eb83f9fe2ad149
João Esperancinha <jofisaes@gmail.com>
2025-03-23 10:06:19 +0100

    installs websockets

--- app/src/main/kotlin/org/jesperancinha/space/Application.kt
+++ app/src/main/kotlin/org/jesperancinha/space/Application.kt
@@ -4,6 +4,7 @@ import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.websocket.*
 import org.jesperancinha.space.config.configureFrameworks
 import org.jesperancinha.space.route.*
 
@@ -15,6 +16,7 @@ fun Application.module() {
     install(ContentNegotiation) {
         json()
     }
+    install(WebSockets)
     configureSerialization()
     configureSecurity()
     configureAdministration()
